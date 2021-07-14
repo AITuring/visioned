c87ff937f63f3b354c2b75b851a06444da5432de
AITuring <shspku365@gmail.com>
2021-07-15 00:09:20 +0800

    第一个graphql搭建完成,虽然之前搞过,但是现在完全记不清了

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 // index.js
 // This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const port = process.env.PORT || 4000;
+// GraphQL编制模式
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+// 为模式字段提供解析函数
+const resolvers = {
+    Query: {
+        hello: () => 'Hello'
+    }
+};
+
+const app = express();
+// 设置Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+// 应用Apollo GraphQL中间件，路径设为/api
+server.applyMiddleware({ app, path: '/api'});
+
+app.listen(port, () => {
+    return console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`)
+})
